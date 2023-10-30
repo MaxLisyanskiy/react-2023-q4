4fc5d73c0179ca5e0b51880929eabf1e3bf213de
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-10-30 17:22:49 +0300

    refactor: change folder name

--- react-basic/vite.config.ts
+++ react-basic/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-2023-q4/module01',
+  base: '/react-2023-q4/react-basic',
 });
