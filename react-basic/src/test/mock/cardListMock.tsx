a9102ee68babda46e21055712ade665f9067490a
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-11-15 21:00:55 +0300

    refactor: remove context and context types

--- react-basic/src/test/mock/cardListMock.tsx
+++ react-basic/src/test/mock/cardListMock.tsx
@@ -1,36 +1,32 @@
-import { BrowserRouter } from 'react-router-dom';
-import { CardsContext } from '../../context/cards-context';
-import { SearchContext } from '../../context/search-context';
-import { ICard } from '../../types/card-type';
-import { PAGE, PAGE_SIZE } from '../../utils/constants';
-import CardList, { CardListProps } from '../../components/CardList/CardList';
+// import { BrowserRouter } from 'react-router-dom';
+// import { CardsContext } from '../../context/cards-context';
+// import { SearchContext } from '../../context/search-context';
+// import { ICard } from '../../types/card-type';
+// import { PAGE, PAGE_SIZE } from '../../utils/constants';
+// import CardList, { CardListProps } from '../../components/CardList/CardList';
 
-export const testCardsData: ICard[] = [
-  {
-    id: '1',
-    name: 'Aargon',
-    images: {
-      small: 'https://images.pokemontcg.io/hgss4/1.png',
-      large: 'https://images.pokemontcg.io/hgss4/1.png',
-    },
-  },
-];
+// export const testCardsData: ICard[] = [
+//   {
+//     id: '1',
+//     name: 'Aargon',
+//     images: {
+//       small: 'https://images.pokemontcg.io/hgss4/1.png',
+//       large: 'https://images.pokemontcg.io/hgss4/1.png',
+//     },
+//   },
+// ];
 
-export const cardListTestProps: CardListProps = {
-  currentPage: PAGE,
-  currentPageSize: PAGE_SIZE,
-  onPageChange: () => {},
-  onPageSizeChange: () => {},
-};
+// export const cardListTestProps: CardListProps = {
+//   currentPage: PAGE,
+//   currentPageSize: PAGE_SIZE,
+//   onPageChange: () => {},
+//   onPageSizeChange: () => {},
+// };
 
-export function generateComponent(cards: ICard[] | []): JSX.Element {
-  return (
-    <BrowserRouter>
-      <SearchContext.Provider value={{ search: '', changeSearch: () => {} }}>
-        <CardsContext.Provider value={{ cards, setCards: () => {} }}>
-          <CardList {...cardListTestProps} />
-        </CardsContext.Provider>
-      </SearchContext.Provider>
-    </BrowserRouter>
-  );
-}
+// export function generateComponent(cards: ICard[] | []): JSX.Element {
+//   return (
+//     <BrowserRouter>
+//       <CardList {...cardListTestProps} />
+//     </BrowserRouter>
+//   );
+// }
--- react-basic/src/test/mock/searchMock.tsx
+++ react-basic/src/test/mock/searchMock.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
-import { CardsContext } from '../../context/cards-context';
-import { SearchContext } from '../../context/search-context';
-import Search from '../../components/Search/Search';
+// import { BrowserRouter } from 'react-router-dom';
+// import { CardsContext } from '../../context/cards-context';
+// import { SearchContext } from '../../context/search-context';
+// import Search from '../../components/Search/Search';
 
-export const SearchMock = (query: string) => {
-  return (
-    <BrowserRouter>
-      <SearchContext.Provider value={{ search: query, changeSearch: () => {} }}>
-        <CardsContext.Provider value={{ cards: [], setCards: () => {} }}>
-          <Search onChangeSearch={() => {}} />
-        </CardsContext.Provider>
-      </SearchContext.Provider>
-    </BrowserRouter>
-  );
-};
+// export const SearchMock = (query: string) => {
+//   return (
+//     <BrowserRouter>
+//       <SearchContext.Provider value={{ search: query, changeSearch: () => {} }}>
+//         <CardsContext.Provider value={{ cards: [], setCards: () => {} }}>
+//           <Search onChangeSearch={() => {}} />
+//         </CardsContext.Provider>
+//       </SearchContext.Provider>
+//     </BrowserRouter>
+//   );
+// };
